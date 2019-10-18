2ddc456d0ca83f548639d30003a4a940583edd3a
Kalyan Gandikota <kalyangandikota@Kalyans-Air.fios-router.home>
2019-10-17 22:21:20 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers';
+import thunk from 'redux-thunk';
+import  Headlines from './components/Headlines'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.store = createStore(rootReducer, applyMiddleware(thunk));
+  }
+  render() {
+    return (
+      <Provider store={this.store}>
+        <Headlines/>
+      </Provider>
+    );
+  }
 }
 
 export default App;
